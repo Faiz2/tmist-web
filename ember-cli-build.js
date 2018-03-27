07c77e2080496cafa61e8ea8e5313c0780a53cca
Alex <891366402@qq.com>
2018-03-28 01:56:00 +0800

    开始迁移

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,20 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    cssModules: {
+      plugins: [
+        require('postcss-import'),
+        require('postcss-extend'),
+        require('postcss-cssnext'),
+        require('rucksack-css')({alias: false,hexRGBA: false,fallbacks: true})
+      ]
+    },
+
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
