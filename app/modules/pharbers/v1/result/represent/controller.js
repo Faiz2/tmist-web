3ed4f11a2384f932e00e45d30eeb4882adb71273
wodelu <1058744843@qq.com>
2018-07-03 14:42:01 +0800

    7.3zhanglu
    差表格没有完成

--- app/modules/pharbers/v1/result/represent/controller.js
+++ app/modules/pharbers/v1/result/represent/controller.js
@@ -3,9 +3,10 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	budgetTip: false,
 	humanTip: false,
+	tabLi:false,
 	hidden(hiddenProperty) {
 		if (this.get(hiddenProperty)) {
-			this.set(hiddenProperty, false)
+			this.set(hiddenProperty, true)
 		}
 	},
 	init() {
@@ -19,9 +20,14 @@ export default Controller.extend({
 			this.hidden('humanTip');
 		},
 		human() {
-			// console.log('human');
+			console.log('human');
 			this.toggleProperty('humanTip');
 			this.hidden('budgetTip');
+		},
+		tab() {
+			console.log('tabLi');
+			this.toggleProperty('tabLi');
+			this.hidden('tabLi');
 		},
 		changeTabs() {
 			console.log(e)
