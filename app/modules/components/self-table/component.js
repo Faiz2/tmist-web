fe321c0f084189ed8be29f11fd7653d4e6dc12d4
WangSen <wsgoodman@163.com>
2018-07-03 14:41:53 +0800

    表格的使用。以及输入区域的样式。
    主要是tab切换功能的实现。

--- app/modules/components/self-table/component.js
+++ app/modules/components/self-table/component.js
@@ -3,27 +3,60 @@ import {
 	computed
 } from '@ember/object';
 import Table from 'ember-light-table';
-// export default Component.extend({
-// });
+
 export default Component.extend({
-	columns: computed(function() {
+
+	columns: computed('columns', function() {
 		return [{
-			label: 'Id',
+			label: '商品名',
 			valuePath: 'id',
-			width: '150px'
+			width: '100px',
+			align: 'center',
+			className: 'testname',
+			sortable: true,
+			sorted: true,
 		}, {
-			label: 'Type',
+			label: '类别',
 			valuePath: 'type',
-			width: '150px'
+			width: '100px',
+			align: 'center',
+
 		}, {
-			label: 'Hit-Points',
-			valuePath: 'healthpoints',
-			width: '150px'
+			label: '治疗领域',
+			valuePath: 'treatmentarea',
+			width: '100px',
+			align: 'center',
+
+		}, {
+			label: '上市时间',
+			valuePath: 'selltime',
+			width: '100px',
+			align: 'center',
+		}, {
+			label: '医保类型',
+			valuePath: 'medicalinsurance',
+			width: '100px',
+			align: 'center',
+		}, {
+			label: '研发类型',
+			valuePath: 'development',
+			width: '100px',
+			align: 'center',
+		}, {
+			label: '公司考核价',
+			valuePath: 'companyprice',
+			width: '100px',
+			align: 'center',
 		}];
 	}),
 
 	table: computed('model', function() {
 		return new Table(this.get('columns'), this.get('model'));
-	})
+	}),
+	actions: {
+		sortByColumn(column) {
+
+		}
+	}
 
 });
