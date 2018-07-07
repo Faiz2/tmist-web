b72cacd45e41e55416c3f2fa3482e77cee1279b9
WangSen <wsgoodman@163.com>
2018-07-07 20:20:23 +0800

    对接后端数据
    对接后端数据，基本完成第一版。Todos： 组件化navbar/overview-number重构。

--- app/modules/pharbers/v1/hosp-detail/controller.js
+++ app/modules/pharbers/v1/hosp-detail/controller.js
@@ -1,66 +1,296 @@
 import Controller from '@ember/controller';
-// import {computed } from '@ember/object';
+import {
+	inject
+} from '@ember/service';
+import {
+	computed
+} from '@ember/object';
 export default Controller.extend({
+	ajax: inject(),
 	budgetTip: false,
 	humanTip: false,
-	baseInfo: true, // 基本信息显示与否
-	news: false, // 医院新闻动态显示与否
-	usePolicy: false, // 用药政策/理念 显示与否
-	selfheight: '500px',
+	showBaseInfo: true, // 基本信息显示与否
+	showNews: false, // 医院新闻动态显示与否
+	showUsePolicy: false, // 用药政策/理念 显示与否
 	targetIncrease: 0,
-	budget: 0,
-
+	budgetCompute: 0,
+	budgetPerc: computed('budget', function() {
+		let budget = this.get('budget');
+		let percent = (budget.used) / (budget.total) * 100;
+		return percent;
+	}),
+	manpowerPerc: computed('manpower', function() {
+		let manpower = this.get('manpower');
+		let newManpower = [];
+		for (let i = 0, len = manpower.length; i < len; i++) {
+			let percentObject = {};
+			percentObject.name = manpower[i].name;
+			percentObject.used = manpower[i].used;
+			percentObject.total = manpower[i].total;
+			percentObject.percent = ((manpower[i].used / manpower[i].total) * 100).toFixed(0);
+			newManpower.push(percentObject);
+		}
+		// this.set('manpower', newManpower);
+		return newManpower;
+	}),
 	hidden(hiddenProperty) {
 		if (this.get(hiddenProperty)) {
 			this.set(hiddenProperty, false)
 		}
 	},
+	getAjaxOpt(data) {
+		return {
+			method: 'POST',
+			dataType: 'json',
+			cache: false,
+			data: JSON.stringify(data),
+			contentType: 'application/json',
+			Accpt: 'application/json',
+		}
+	},
+	sendAjax(url, condition, value) {
+		this.get('ajax').request(url, this.getAjaxOpt(condition))
+			.then(({
+				status,
+				result,
+				error,
+			}) => {
+				if (status === "ok") {
+					// console.log(result.data.attribute);
+					this.set(value, result.data.attribute)
+				} else {
+					this.set('errorMessage', error.message);
+				}
+			}, () => {})
+	},
+	/**
+	 * 查询医院详情
+	 */
+	queryDetail() {
+		let condition = {
+			"token": "token123456789",
+			"timestamp": 1530689119000,
+			"version": {
+				"major": 1,
+				"minor": 0
+			},
+			"data": {
+				"type": "hosp_detail",
+				"condition": {
+					"scenario_id": "scenario_id"
+				}
+			}
+		};
+		this.get('ajax').request('/api/scenario/hospital/detail', this.getAjaxOpt(condition))
+			.then(({
+				status,
+				result,
+				error
+			}) => {
+				if (status === 'ok') {
+					// console.info(result);
+					this.set('medicines', result.data.attribute.medicines);
+					// this.set('hospitalInfo', result.data.attribute.hospital);
+				} else {
+					this.set('errorMessage', error.message)
+				}
+			}, () => {})
+	},
+	/**
+	 *	查询budget
+	 */
+	queryBudget() {
+		let condition = {
+			"token": "token123456789",
+			"timestamp": 1530689119000,
+			"version": {
+				"major": 1,
+				"minor": 0
+			},
+			"data": {
+				"type": "budget_progress",
+				"condition": {
+					"scenario_id": "scenario_id"
+				}
+			}
+		};
+		let url = '/api/scenario/budget/info';
+		let value = 'budget';
+		this.sendAjax(url, condition, value);
+
+	},
+	/**
+	 *	查询人力
+	 */
+	queryManpower() {
+		let condition = {
+			"token": "token123456789",
+			"timestamp": 1530689119000,
+			"version": {
+				"major": 1,
+				"minor": 0
+			},
+			"data": {
+				"type": "budget_progress",
+				"condition": {
+					"scenario_id": "scenario_id"
+				}
+			}
+		}
+		let url = 'api/scenario/humans/info';
+		let value = 'manpower';
+		this.sendAjax(url, condition, value);
+	},
 	init() {
 		this._super(...arguments);
+		this.budget = {};
+		this.manpower = [];
+		this.queryDetail();
+		this.queryBudget();
+		this.queryManpower();
+		this.hospitalInfo = {
+			"id": "idididi",
+			"name": "中日医院",
+			"basicinfo": [{
+				'key': '医院类型',
+				"value": "综合",
+			}, {
+				'key': '医院等级',
+				'value': '三甲'
+			}, {
+				'key': '病床数量',
+				'value': 4562,
+			}, {
+				'key': '特色科室',
+				'value': '皮肤科'
+			}, {
+				'key': '门诊人次/年',
+				'value': 4612,
+			}, {
+				'key': '手术台数',
+				'value': 68755
+			}, {
+				'key': '住院人数/年',
+				'value': 1000,
+			}],
+			"news": {},
+			"policy": {}
+		};
 		this.medicines = [{
-				'id': 'medicine000',
-				'href': '#medicine000',
-				'name': '口服抗生素',
-				'marketpotential': 333444555,
-				'potentialgrowth': 99,
-				'previoussales': 555444,
-				'previousgrowth': 99,
-				'share': 12,
-				'contributionrate': 4,
-				'detail': {
-					'id': 'medicine01_detail',
-					'value': []
+				"id": "medicine000",
+				"name": "口服抗生素",
+				"marketpotential": 333444555,
+				"potentialgrowth": 99,
+				"previoussales": 555444,
+				"previousgrowth": 99,
+				"share": 12,
+				"contributionrate": 4,
+				"detail": {
+					"id": "medicine000_detail",
+					"value": [{
+						"id": "霉素",
+						"type": "口服抗生素",
+						"treatmentarea": "抗生素",
+						"selltime": "2000",
+						"medicalinsurance": "甲类",
+						"development": "首仿",
+						"companyprice": 44
+					}]
 				},
-				'hospital': {
-					'id': 'medicine01_hospital',
-					'value': []
+				"history": {
+					"id": "medicine000_history",
+					"value": [{
+						"time": "星期一",
+						"representative": "小三",
+						"timemanagement": "12天",
+						"budgetallocation": 5000,
+						"budgetratio": 5000,
+						"indicator": 50000,
+						"growth": 1.23,
+						"achievementrate": 95
+					}]
 				},
-				'competitionproducts': {
-					'id': 'medicine01_competitionproducts',
-					'value': []
+				"competitionproducts": {
+					"id": "medicine000_competitionproducts",
+					"value": []
 				}
 			},
 			{
-				'id': 'medicine111',
-				'href': '#medicine111',
-				'name': '止痛药',
-				'marketpotential': 333444555,
-				'potentialgrowth': 99,
-				'previoussales': 555444,
-				'previousgrowth': 99,
-				'share': 12,
-				'contributionrate': 4,
-				'detail': {
-					'id': 'medicine02_detail',
-					'value': []
+				"id": "medicine001",
+				"name": "口服护发素",
+				"marketpotential": 333444555,
+				"potentialgrowth": 99,
+				"previoussales": 555444,
+				"previousgrowth": 99,
+				"share": 12,
+				"contributionrate": 4,
+				"detail": {
+					"id": "medicine001_detail",
+					"value": [{
+						"id": "霉素",
+						"type": "口服抗生素",
+						"treatmentarea": "抗生素",
+						"selltime": "2000",
+						"medicalinsurance": "甲类",
+						"development": "首仿",
+						"companyprice": 44
+					}]
 				},
-				'hospital': {
-					'id': 'medicine02_hospital',
-					'value': []
+				"history": {
+					"id": "medicine001_history",
+					"value": [{
+						"time": "星期一",
+						"representative": "小三",
+						"timemanagement": "12天",
+						"budgetallocation": 5000,
+						"budgetratio": 5000,
+						"indicator": 50000,
+						"growth": 1.23,
+						"achievementrate": 95
+					}]
 				},
-				'competitionproducts': {
-					'id': 'medicine02_competitionproducts',
-					'value': []
+				"competitionproducts": {
+					"id": "medicine001_competitionproducts",
+					"value": []
+				}
+			},
+			{
+				"id": "medicine002",
+				"name": "口服叶绿素",
+				"marketpotential": 333444555,
+				"potentialgrowth": 99,
+				"previoussales": 555444,
+				"previousgrowth": 99,
+				"share": 12,
+				"contributionrate": 4,
+				"detail": {
+					"id": "medicine002_detail",
+					"value": [{
+						"id": "霉素",
+						"type": "口服抗生素",
+						"treatmentarea": "抗生素",
+						"selltime": "2000",
+						"medicalinsurance": "甲类",
+						"development": "首仿",
+						"companyprice": 44
+					}]
+				},
+				"history": {
+					"id": "medicine002_history",
+					"value": [{
+						"time": "星期一",
+						"representative": "小三",
+						"timemanagement": "12天",
+						"budgetallocation": 5000,
+						"budgetratio": 5000,
+						"indicator": 50000,
+						"growth": 1.23,
+						"achievementrate": 95
+					}]
+				},
+				"competitionproducts": {
+					"id": "medicine002_competitionproducts",
+					"value": []
 				}
 			}
 		];
@@ -112,21 +342,21 @@ export default Controller.extend({
 		this.columnsHospitalValue = [{
 			time: '星期一',
 			representative: '小三',
-			timemanagement: '12天',
-			budgetallocation: '5,000',
-			budgetratio: '5,000',
-			indicator: '50,000',
-			growth: '1.23',
-			achievementrate: '95%',
+			timemanagement: 12,
+			budgetallocation: 5000,
+			budgetratio: 5000,
+			indicator: 50000,
+			growth: 123,
+			achievementrate: 95,
 		}, {
 			time: '星期一',
 			representative: '小三',
-			timemanagement: '12天',
-			budgetallocation: '5,000',
-			budgetratio: '5,000',
-			indicator: '50,000',
-			growth: '1.23',
-			achievementrate: '95%',
+			timemanagement: 15,
+			budgetallocation: 55000,
+			budgetratio: 7000,
+			indicator: 57000,
+			growth: 1.5,
+			achievementrate: 92,
 		}, ];
 	},
 	actions: {
@@ -147,19 +377,19 @@ export default Controller.extend({
 			this.transitionToRoute('pharbers.v1.hospital')
 		},
 		changeTabsInfo() {
-			this.set('baseInfo', true);
-			this.set('news', false);
-			this.set('usePolicy', false);
+			this.set('showBaseInfo', true);
+			this.set('showNews', false);
+			this.set('showUsePolicy', false);
 		},
 		changeTabsNews() {
-			this.set('baseInfo', false);
-			this.set('news', true);
-			this.set('usePolicy', false);
+			this.set('showBaseInfo', false);
+			this.set('showNews', true);
+			this.set('showUsePolicy', false);
 		},
 		changeTabsPolicy() {
-			this.set('baseInfo', false);
-			this.set('news', false);
-			this.set('usePolicy', true);
+			this.set('showBaseInfo', false);
+			this.set('showNews', false);
+			this.set('showUsePolicy', true);
 		},
 	}
 });
